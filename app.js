4d824deb56e034cd79b0fc0ceee8187bc5b9b75f
Nicko Guyer <nguyer@gmail.com>
2015-02-10 18:44:42 +0000

    Added sourceIp option

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var from = '19194181235';
 var ip = '127.0.0.1';
 var port = 5060;
 var sourcePort = 6161;
+var sourceIp = '127.0.0.1';
 
 for (var i = 2; i < process.argv.length; i++) {
 	if (process.argv[i] === '--to') {
@@ -32,9 +33,16 @@ for (var i = 2; i < process.argv.length; i++) {
 		i++;
 		sourcePort = process.argv[i];
 	}
+	else if (process.argv[i] === '--sourceIp') {
+		i++;
+		sourceIp = process.argv[i];
+	}
 }
 
-var sipClient = sip.create({ port : sourcePort });
+var sipClient = sip.create({
+	port          : sourcePort,
+	publicAddress : sourceIp
+});
 
 function rstring() { return Math.floor(Math.random() * 1e6).toString(); }
 
@@ -43,11 +51,11 @@ var invite = {
 	uri : 'sip:+' + to + '@' + ip + ':' + port,
 	headers: {
 		to : { uri : 'sip:+' + to + '@' + ip + ':' + port },
-		from : { uri: 'sip:+' + from + '@' + os.hostname() + ':' + sourcePort, params : { tag : rstring() } },
+		from : { uri: 'sip:+' + from + '@' + sourceIp + ':' + sourcePort, params : { tag : rstring() } },
 		'call-id': rstring(),
 		cseq: { method : 'INVITE', seq: Math.floor(Math.random() * 1e5) },
 		'content-type': 'application/sdp',
-		contact : [ { uri: 'sip:+' + from + '@' + os.hostname() + ':' + sourcePort } ]
+		contact : [ { uri: 'sip:+' + from + '@' + sourceIp + ':' + sourcePort } ]
 	}
 };
 
